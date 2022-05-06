a4a6b4af8dbd991e733de41d5868349d7037ee83
something972 <104917826+something972@users.noreply.github.com>
2022-05-06 11:29:10 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,5 @@
 import turtle
 
-#car
-car = turtle.Turtle()
-car.shape("square")
-car.color("indian red")
-car.shapesize(stretch_wid = 4, stretch_len=10)
-
 #window Setup
 wn = turtle.Screen()
 wn.title("ratio of gasoline burnt and time spent")
@@ -13,20 +7,91 @@ wn.bgcolor("dark slate gray")
 wn.setup(width=800,height=600)
 wn.tracer(0)
 
+# Below code for drawing rectangular upper body
+car = turtle.Turtle()
+car.color('#008000')
+car.fillcolor('#008000')
+car.penup()
+car.goto(0,0)
+car.pendown()
+car.begin_fill()
+car.forward(370)
+car.left(90)
+car.forward(50)
+car.left(90)
+car.forward(370)
+car.left(90)
+car.forward(50)
+car.end_fill()
+  
+   
+# Below code for drawing window and roof
+car.penup()
+car.goto(100, 50)
+car.pendown()
+car.setheading(45)
+car.forward(70)
+car.setheading(0)
+car.forward(100)
+car.setheading(-45)
+car.forward(70)
+car.setheading(90)
+car.penup()
+car.goto(200, 50)
+car.pendown()
+car.forward(49.50)
+  
+# Below code for drawing two tyres
+car.penup()
+car.goto(100, -10)
+car.pendown()
+car.color('#000000')
+car.fillcolor('#000000')
+car.begin_fill()
+car.circle(20)
+car.end_fill()
+car.penup()
+car.goto(300, -10)
+car.pendown()
+car.color('#000000')
+car.fillcolor('#000000')
+car.begin_fill()
+car.circle(20)
+car.end_fill()
+car.hideturtle()
+x = car.xcor()
+
+#functions
+def car_right():
+    x = car.xcor()
+    x = x - 20 
+    car.setx(x) 
+
+def car_left():
+    x = car.xcor()
+    x = x + 20 
+    car.setx(x)
+
+#keyboard bindings
+wn.listen()
+wn.onkeypress(car.left, "a")
+wn.onkeypress(car.right, "d")
+
 gallons = 0
 minutes = 0
 
 #write
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Gallons of gasoline used: 0 Time in minutes spent: 0", align= "center", font=("Corier", 24, "normal"))
+write = turtle.Turtle()
+write.speed(0)
+write.goto(0,260)
+write.shape("square")
+write.color("white")
+write.penup()
+write.hideturtle()
+write.write("Gallons of gasoline used: 0 Time in minutes spent: 0", align= "center", font=("Corier", 24, "normal"))
 
 while True:
     if minutes == 1:
         gallons = gallons + 0.00333333333 
-        pen.write("Gallons of gasoline used: {} Time in minutes spent: {}".format(gallons,minutes), align = "center", font= ("Corier", 24, "normal" )) 
+        write.write("Gallons of gasoline used: {} Time in minutes spent: {}".format(gallons,minutes), align = "center", font= ("Corier", 24, "normal" )) 
+
