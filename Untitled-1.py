dff5594c971856b1a969ba06224b6569085506bf
CrashGibson613 <104917049+CrashGibson613@users.noreply.github.com>
2022-05-06 10:33:41 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,7 @@
 import turtle
 
 #car
-car = turtle.Turtle
+car = turtle.Turtle()
 car.shape("square")
 car.color("indian red")
 car.shapesize(stretch_wid = 4, stretch_len=10)
@@ -9,7 +9,7 @@ car.shapesize(stretch_wid = 4, stretch_len=10)
 #window Setup
 wn = turtle.Screen()
 wn.title("ratio of gasoline burnt and time spent")
-wn.color("dark slate gray")
+wn.bgcolor("dark slate gray")
 wn.setup(width=800,height=600)
 wn.tracer(0)
 
@@ -17,12 +17,16 @@ gallons = 0
 minutes = 0
 
 #write
-write = turtle.Turtle()
-write.goto(0,260)
-write.penup()
-write.write("Gallons of gasoline used: 0 Time in minutes spent: 0", align= "center", font=("Corier", 24, "normal"))
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Gallons of gasoline used: 0 Time in minutes spent: 0", align= "center", font=("Corier", 24, "normal"))
 
-if minutes == 1:
-    gallons = gallons + 0.00333333333 
-    write.write("Gallons of gasoline used: {} Time in minutes spent: {}".format(gallons,minutes), align = "center", font= ("Corier", 24, "normal" )) 
-
+while True:
+    if minutes == 1:
+        gallons = gallons + 0.00333333333 
+        pen.write("Gallons of gasoline used: {} Time in minutes spent: {}".format(gallons,minutes), align = "center", font= ("Corier", 24, "normal" )) 
